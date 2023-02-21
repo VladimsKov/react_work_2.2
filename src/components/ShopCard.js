7897adc268059de3bf548180a264c7d8d404105c
VladimsKov <vladimskv@yandex.ru>
2023-02-21 23:44:45 +0300

    corr.

--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -6,7 +6,7 @@ export default function ShopCard({ name, price, color, imgSrc }) {
       <div className="card-name">{name}</div>
       <div className="card-color">{color}</div>
       </div>      
-<img className="card-img" src = {imgSrc} alt="" />
+<img className="card-img" src = {imgSrc} alt=" " />
       <div className="card-block">
       <div>${price}</div>
       <button className="card-btn">ADD TO CART</button>  
